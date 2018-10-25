0d48d745266ec5225ff3d335e0ec42137fc71faa
baowen.tang <1836791144@qq.com>
2018-10-25 16:06:03 +0800

    扫码购整合在线更新（更新json下载路径10/25）

--- app/src/main/java/com/snoopy/scancode/MainActivity.java
+++ app/src/main/java/com/snoopy/scancode/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity implements ScanGunHelper.OnS
         homeAppPage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                getQRCodeInfo("2018040864", "0FDA26ECB6E3E5F56C109522F96BB777");
+                //getQRCodeInfo("2018040864", "0FDA26ECB6E3E5F56C109522F96BB777");
             }
         });
         getSupportActionBar().hide();
